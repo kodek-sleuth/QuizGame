8a5c3c9d811e6845d67efa50c10f24cb9234aef5
Mugerwa Joseph <josekodek2000@gmail.com>
2020-02-17 18:36:18 +0200

    third commit

--- main.go
+++ main.go
@@ -4,24 +4,17 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
-	_"io"
+	_ "io"
 	"log"
 	"os"
 	"time"
 )
 
-func errorHandler(err error){
+func errorHandler(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 	return
-}
-
-func timer(){
-	var starter string
-	fmt.Println("Press any key to start")
-	_, err := fmt.Scan(&starter)
-	errorHandler(err)
 }
 
 func scanAnswers(record string, expectedAnswer string) string {
@@ -38,31 +31,59 @@ func scanAnswers(record string, expectedAnswer string) string {
 	return ""
 }
 
-func makeScore(arr []string, questions int){
+func makeScore(arr []string, questions int) {
 	answerCount := 0
 	for _ = range arr {
 		answerCount += 1
 	}
-	fmt.Println("Passed ", answerCount, " out of ", questions)
+	fmt.Println("\nPassed ", answerCount, " out of ", questions)
 }
 
-func main(){
-	var correct []string
+func readLines() int{
 	f, err := os.OpenFile("problems.csv", os.O_RDWR|os.O_CREATE, 0755) // Open file
 	errorHandler(err)
 	read := csv.NewReader(f)
-	questionCount := 0
-	for {
-		record, err := read.Read()
-		if err == io.EOF {
-			break
-		}
-		if answer := scanAnswers(record[0], record[1]); answer != "" {
-			correct = append(correct, answer)
-		}
-		questionCount += 1
+	csvFile, err := read.ReadAll()
+	errorHandler(err)
+	return len(csvFile)
+}
+
+func main() {
+	var correct []string
+	var enter string
+
+	fmt.Println("Press s and the enter key to start")
+	_, err := fmt.Scan(&enter)
+	errorHandler(err)
+
+	timer1 := time.NewTimer(10 * time.Second) // return channel
+	defer timer1.Stop() // stop after main func execution
+	done := make(chan bool) // create a channel
+	go func(chanel chan bool) {
+		f, err := os.OpenFile("problems.csv", os.O_RDWR|os.O_CREATE, 0755) // Open file
 		errorHandler(err)
+		read := csv.NewReader(f)
+		for {
+			record, err := read.Read()
+			if err == io.EOF {
+				break
+			}
+			if answer := scanAnswers(record[0], record[1]); answer != "" {
+				correct = append(correct, answer)
+			}
+			errorHandler(err)
+		}
+		f.Close()
+		done <- true
+		makeScore(correct, readLines())
+	}(done)
+	for {
+		select {
+		case <-done:
+			return
+		case <-timer1.C:
+			makeScore(correct, readLines())
+			return
+		}
 	}
-	f.Close()
-	makeScore(correct, questionCount)
 }
